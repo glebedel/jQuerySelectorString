3f2952298aa67849cb4ac671a7f57f4f58ca6a19
glebedel <guillaume.lebedel@gmail.com>
2015-06-15 23:25:16 +0100

    Update Documentation + Minify version

--- SelectorString.js
+++ SelectorString.js
@@ -1,11 +1,27 @@
+/**
+ * @file Manages the configuration settings for the widget.
+ * @author Guillaume Lebedel
+ * @version 0.3 
+ */
+
+//Extend the jQuery object with our SelectorString object
 jQuery.extend(jQuery, {
     SelectorString: {
+        //enable or disable the use of classes in selector paths
         useClasses: true,
+        //stores all the classes we want to exclude from selector paths
         excludedClasses: [],
-
+        /**
+         * add a class string to the excluded classes array
+         * @param {string} classString the string of the class we want to be excluded from selector paths
+         */
         addExcludedClass: function(classString) {
             this.excludedClasses.push(classString);
         },
+        /**
+         * remove a class from the excluded class array
+         * @param  {string} classString string of the class we don't want to exclude anymore
+         */
         removeExcludedClass: function(classString) {
             this.excludedClasses.splice(this.excludedClasses.indexOf(classString), 1);
         }
@@ -34,7 +50,7 @@ jQuery.fn.extend({
             var parent = node.parent();
             //gets the best selector of a node relative to its parent
             name = this.getSelectorToParent.apply(node, [parent]);
-            //if (node !== this && path && name[name.length - 1] === ")") name += " :scope";
+            //if (node !== this && path && name[name.length - 1] === ")") name += " :scope"; // this is strictly for the use of eq:() parsing within querySelectorAll
             path = name + (path ? ' > ' + path : '');
             node = parent;
         }
@@ -53,40 +69,34 @@ jQuery.fn.extend({
             selector = domNode.localName,
             classes = [],
             childResults = parent.children(selector);
-            //copy the node classes to a new string array
+        //copy the node classes to a new string array
         domNode.classList.forEach(function(value) {
             classes.push(value);
         });
         //Find the class combination which will return the less nodes
         if (useClasses)
-            //check the best class combination until we haven't found one that returns a unique node*/
+        //check the best class combination until we haven't found one that returns a unique node*/
             for (var bestClassIndex = 0, bestResult = childResults.length; classes.length && bestResult > 1 && bestClassIndex >= 0;) {
-                bestClassIndex = -1;
-                //find the class which identifies our node the best comapred to its parent
-                for (var i = 0; i < classes.length && bestResult > 1; i++) {
-                    if (classes[i].length === 0 || jQuery.SelectorString.excludedClasses.indexOf(classes[i]) > -1) break;
-                    childResults = parent.children(selector + "." + classes[i]);
-                    if (childResults.length < bestResult) {
-                        bestResult = childResults.length;
-                        bestClassIndex = i;
-                    }
-                }
-                if (bestClassIndex >= 0) {
-                    //since we found a class improving the selector we add it to the result
-                    selector += "." + classes[bestClassIndex];
-                    //delete that class from the class array
-                    classes.splice(bestClassIndex, 1);
-                    i = 0;
+            bestClassIndex = -1;
+            //find the class which identifies our node the best comapred to its parent
+            for (var i = 0; i < classes.length && bestResult > 1; i++) {
+                if (classes[i].length === 0 || jQuery.SelectorString.excludedClasses.indexOf(classes[i]) > -1) break;
+                childResults = parent.children(selector + "." + classes[i]);
+                if (childResults.length < bestResult) {
+                    bestResult = childResults.length;
+                    bestClassIndex = i;
                 }
             }
+            if (bestClassIndex >= 0) {
+                //since we found a class improving the selector we add it to the result
+                selector += "." + classes[bestClassIndex];
+                //delete that class from the class array
+                classes.splice(bestClassIndex, 1);
+                i = 0;
+            }
+        }
         childResults = parent.children(selector);
         //if the selector doesn't give us a unique node we add the :eq
         return selector + (childResults.length > 1 ? ":eq(" + childResults.index(this) + ")" : "");
     }
 });
-
-
-
-
-//Activate Selector Prompt
-//Display Prompt icon on screen while live
